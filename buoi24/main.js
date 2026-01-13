71f44f61220e2d25e6c708a25a3d7e15d74e57bf
Trần Chí Khang <akaimessikhang@gmail.com>
2026-01-13 13:56:04 +0700

    push buoi24

---
+++ buoi24/main.js
@@ -0,0 +1,35 @@
+function isPrime(n) {
+    if (n <= 1) return false;
+
+    for (let i = 2; i <= n ** 0.5; i++) {
+        if (n % i === 0) {
+            console.log(n + " không phải số nguyên tố");
+            return false;
+        }
+    }
+    console.log(n + " là số nguyên tố");
+
+    return true;
+}
+console.log(isPrime(2));
+console.log(isPrime(7));
+console.log(isPrime(9));
+console.log(isPrime(1));
+
+// Test 2=============================================================
+function isPerfectNumber(n) {
+    if (n <= 1) return false;
+
+    let sum = 0;
+
+    for (let i = 1; i <= n / 2; i++) {
+        if (n % i === 0) {
+            sum += i;
+        }
+    }
+
+    return sum === n;
+}
+console.log(isPerfectNumber(6));
+console.log(isPerfectNumber(28));
+console.log(isPerfectNumber(12));
