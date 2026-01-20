bf456a17b64e16227a0b09ac6dac6bb6c428af47
Trần Chí Khang <akaimessikhang@gmail.com>
2026-01-20 19:03:42 +0700

    home work 25

---
+++ buoi25/main.js
@@ -0,0 +1,67 @@
+const products = [
+    { id: 1, name: "iPhone", price: 2000 },
+    { id: 2, name: "Samsung", price: 1500 },
+    { id: 3, name: "Xiaomi", price: 1000 },
+    { id: 4, name: "Oppo", price: 1200 },
+];
+
+const orders = [
+    {
+        id: 1,
+        items: [
+            { productId: 1, quantity: 2 },
+            { productId: 2, quantity: 1 },
+        ],
+    },
+    {
+        id: 2,
+        items: [
+            { productId: 1, quantity: 1 },
+            { productId: 3, quantity: 3 },
+        ],
+    },
+    {
+        id: 3,
+        items: [
+            { productId: 2, quantity: 2 },
+            { productId: 4, quantity: 1 },
+        ],
+    },
+];
+
+const getBestSellingProduct = (products, orders) => {
+    let revenue = {};
+    let maxRevenue = 0;
+    let bestProductName = "";
+    for (const order of orders) {
+        for (const item of order.items) {
+            for (const product of products) {
+                if (product.id === item.productId) {
+                    const money = product.price * item.quantity;
+                    if (!revenue[product.id]) {
+                        revenue[product.id] = money;
+                    } else {
+                        revenue[product.id] += money;
+                    }
+                }
+            }
+        }
+    }
+    let bestProductId = null;
+    for (const productId in revenue) {
+        if (revenue[productId] > maxRevenue) {
+            maxRevenue = revenue[productId];
+            bestProductId = productId;
+        }
+    }
+    for (const product of products) {
+        if (product.id === Number(bestProductId)) {
+            bestProductName = product.name;
+            break;
+        }
+    }
+
+    return bestProductName;
+};
+const revenueResult = getBestSellingProduct(products, orders);
+console.log(revenueResult);
